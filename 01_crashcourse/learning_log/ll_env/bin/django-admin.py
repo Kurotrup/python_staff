62afd2f5079ce10b50284cadc54afc2ffe0f479e
kur <kur@localhost.localdomain>
2018-12-09 13:03:55 +0200

    Started django project

---
+++ 01_crashcourse/learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kur/github/python_staff/01_crashcourse/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
